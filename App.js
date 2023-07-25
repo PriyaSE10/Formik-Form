3e779b3e5842ce183d3c9a099954567ff317d6c4
Priya <140314796+PriyaSE10@users.noreply.github.com>
2023-07-25 21:15:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css'
+import FormikForm from "./Components/Formik/FormikForm";
+
+function App() {
+ return(<div>
+  <FormikForm/>
+  
+ </div>)
+}
+export default App;
